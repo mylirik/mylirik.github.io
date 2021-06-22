295e998990374407c0f14f8184bddf4e01ed4326
siregararistoteles <aristotelessiregar08@gmail.com>
2021-06-22 20:04:16 +0700

    Second Init

---
+++ assets/js/TheBone.js
@@ -0,0 +1,45 @@
+
+// Musik Player
+var player = document.querySelector('#player');
+let progress = document.querySelector('#progress');
+let playbtn = document.querySelector('#playbtn');
+
+var playpause = function () {
+    if (player.paused) {
+        player.play();
+    } else {
+        player.pause();
+    }
+}
+
+playbtn.addEventListener('click', playpause);
+player.onplay = function () {
+    playbtn.classList.remove('fa-play');
+    playbtn.classList.add('fa-pause');
+}
+
+player.onpause = function () {
+    playbtn.classList.add('fa-play');
+    playbtn.classList.remove('fa-pause');
+}
+
+player.ontimeupdate = function () {
+    let ct = player.currentTime;
+    current.innerHTML = timeFormat(ct);
+    // progress
+    let duration = player.duration;
+    prog = Math.floor((ct * 100) / duration);
+    progress.style.setProperty("--progress", prog + "%");
+}
+
+function timeFormat (ct) {
+    minutes = Math.floor(ct / 60);
+    seconds = Math.floor(ct % 60);
+
+    if (seconds < 10) {
+        seconds = "0"+seconds;
+    }
+
+    return minutes + ":" + seconds;
+}
+// Akhir Musik Player
